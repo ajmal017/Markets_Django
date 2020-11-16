ff0221bd785e7ffc123338d7e1db42c9d222462c
pravvvv <praveensmallya@gmail.com>
2020-11-16 16:39:46 +0530

    updated index.html and views.py for sector analysis

--- sector_analysis/views.py
+++ sector_analysis/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+import json
 sectors = ["FMCG", "IT", "BANKING"]
+dict={}
+dict["FMCG"] = ["BRITANNIA","ITC","HINDUNILVR","MARICO"]
+
+
 # Create your views here.
 def index(request):
     return render(request,"sector_analysis/index.html",
-    {"sectors": sectors}
+    {"sectors": sectors,
+    "dict":json.dumps(dict)}
     )
