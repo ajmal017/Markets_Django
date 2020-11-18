84638960a09f82c346857dd774657f9d47f86cb0
pravvvv <praveensmallya@gmail.com>
2020-11-18 23:22:45 +0530

    Completed adding calculation bars , risk-reward values , added add to portfolio button

--- sector_analysis/models.py
+++ sector_analysis/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Holdings(models.Model):
+    tikr = models.CharField(max_length=64)
+    buy = models.FloatField()
+    sl = models.FloatField()
+    qty = models.IntegerField()
--- sector_analysis/views.py
+++ sector_analysis/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 import json
+import pandas as pd
+from bokeh.plotting import figure, output_file, show 
+from bokeh.embed import components
+
+mdf_15m = pd.read_csv(r'C:\Users\prave\Documents\GitHub\Market-Analysis\mdf_15m.csv')
+mdf_15m = pd.read_csv(r'C:\Users\prave\Documents\GitHub\Market-Analysis\mdf_15m.csv')
+mdf_15m = pd.read_csv(r'C:\Users\prave\Documents\GitHub\Market-Analysis\mdf_15m.csv')
+
 sector_img_path = '\\sector_analysis\\static\\sector_analysis\\images'
 def get_sectors_list(sector_img_path):
 	import os
@@ -38,10 +46,26 @@ from os.path import isfile, join
 # Create your views here.
 
 def index(request):
+
+    # x= [1,3,5,7,9,11,13]
+    # y= [1,2,3,4,5,6,7]
+    # title = 'y = f(x)'
+
+    # plot = figure(title= title , 
+    #     x_axis_label= 'X-Axis', 
+    #     y_axis_label= 'Y-Axis', 
+    #     plot_width =400,
+    #     plot_height =400)
+
+    # plot.line(x, y, legend= 'f(x)', line_width = 2)
+    # #Store components 
+    # script, div = components(plot)
+
+
     return render(request,"sector_analysis/index.html",
     {"sectors": sectors,
     "dict":json.dumps(dict),
-    "img_path":sectors
+    "img_path":sectors,
     }
     )
 
