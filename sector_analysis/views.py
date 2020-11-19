d8e38decf7fd87fc2103ed09d600e83cbd753192
pravvvv <praveensmallya@gmail.com>
2020-11-19 14:28:27 +0530

    started new branch from yesterday

--- sector_analysis/views.py
+++ sector_analysis/views.py
@@ -18,14 +18,8 @@ def get_sectors_list(sector_img_path):
 	sectors = [x.split(s+'\\')[1] for x in sectors]
 	return sectors
 sectors = get_sectors_list(sector_img_path)
-# sectors = ["FMCG", "IT", "BANKING"]
-# dict={}
-# dict["FMCG"] = ["BRITANNIA","ITC","HINDUNILVR","MARICO"]
 import os
 from django.templatetags.static import static
-
-
-import os
 s = os.getcwd()+sector_img_path+'\\'
 dict = {}
 sectors_filenames = [x[0] for x in os.walk(s)][1:]
@@ -37,34 +31,11 @@ for text in sectors_filenames:
     companies = [x.split('.png')[0] for x in companies]
     dict[sector] = companies
 
-# import settings
-# url = settings.STATIC_URL   
-# from os import listdir
-from os.path import isfile, join
-# fldrs = [static(f) for f in listdir(url) if isfile(join(mypath, f))]
-# url = isfile(''.join(url))
-# Create your views here.
-
 def index(request):
-
-    # x= [1,3,5,7,9,11,13]
-    # y= [1,2,3,4,5,6,7]
-    # title = 'y = f(x)'
-
-    # plot = figure(title= title , 
-    #     x_axis_label= 'X-Axis', 
-    #     y_axis_label= 'Y-Axis', 
-    #     plot_width =400,
-    #     plot_height =400)
-
-    # plot.line(x, y, legend= 'f(x)', line_width = 2)
-    # #Store components 
-    # script, div = components(plot)
-
-
+    
     return render(request,"sector_analysis/index.html",
     {"sectors": sectors,
-    "dict":json.dumps(dict),
+    "dict":json.dumps(dict) ,
     "img_path":sectors,
     }
     )
