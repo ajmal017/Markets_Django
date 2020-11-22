27a77a8b2db32f0605b6171ef975f6ef8e5fd0a9
pravvvv <praveensmallya@gmail.com>
2020-11-23 00:54:59 +0530

    Added My Portfolio with delete buttons , linked to SQLite database

--- sector_analysis/models.py
+++ sector_analysis/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class ltp_tikrs(models.Model):
-    tikr = models.CharField(max_length=64)
-    ltp = models.FloatField()
+    tikr = models.CharField(max_length=64,primary_key = True)
+    ltp = models.FloatField(default=0.0)
+    def __str__(self):
+        return f"{self.tikr}|{self.ltp}"
 
+
+from datetime import datetime
 # Create your models here.
 class Holdings(models.Model):
     tikr = models.CharField(max_length=64)
@@ -11,8 +15,8 @@ class Holdings(models.Model):
     sl = models.FloatField()
     qty = models.IntegerField()
     t = models.FloatField()
-    # entrydate = models.DateTimeField()
+    entrydate = models.DateTimeField(default=datetime.now, blank=True)
     # dt = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.tikr}|{self.buy}|{self.qty}|{self.sl}|{self.t}"
+        return f"{self.tikr}|{self.buy}|{self.qty}|{self.sl}|{self.t}|{self.entrydate}"
--- sector_analysis/views.py
+++ sector_analysis/views.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 from bokeh.plotting import figure, output_file, show 
 from bokeh.embed import components
-from sector_analysis.models import Holdings
+from sector_analysis.models import Holdings,ltp_tikrs
 import time
 mdf_15m = pd.read_csv(r'C:\Users\prave\Documents\GitHub\Market-Analysis\mdf_15m.csv')
 mdf_15m = pd.read_csv(r'C:\Users\prave\Documents\GitHub\Market-Analysis\mdf_15m.csv')
@@ -38,26 +38,42 @@ for text in sectors_filenames:
     dict[sector] = companies
 
 holdings = Holdings.objects.all()
+def find_ltp_render(holdings):
+    import yfinance as yf
+    tikr_list =list(holdings.tikr.value_counts().index)
+    dict={}
+    ltp_tikrs_ = ltp_tikrs.objects.all()
+    for key in tikr_list:
+        try:
+            obj = ltp_tikrs_.objects.get(tikr=key)
+            dict[key] = obj.ltp
+        except:
+            print('yf render')
+            obj = ltp_tikrs.objects.create(tikr=key)
+            obj.ltp =round(yf.download(key+".NS",period="5d",interval="1d").dropna().tail(1)['Close'][0],2)
+            dict[key]=obj.ltp     
+            time.sleep(1)
+    return dict
 
 def find_ltp(holdings):
     import yfinance as yf
     tikr_list =list(holdings.tikr.value_counts().index)
     dict={}
-    c=150
     for key in tikr_list:
         try:
-            print('parse')
-            dict[key]=parsePrice(key+".NS")
-            time.sleep(1)
-        except:
-        # try:
+            obj = ltp_tikrs.objects.get(tikr=key)            
             print('yf')
             dict[key]=round(yf.download(key+".NS",period="5d",interval="1d").dropna().tail(1)['Close'][0],2)
+            obj.ltp = dict[key]
+            obj.save()
             time.sleep(1)
-        # except:
-        #     print('parse')
-        #     dict[key]=parsePrice(key+".NS")
-        #     time.sleep(1)
+        except:
+            obj = ltp_tikrs.objects.get(tikr=key) 
+            print('parse')
+            dict[key]=parsePrice(key+".NS")
+            obj.ltp = dict[key]
+            obj.save()
+            time.sleep(1)
     return dict
 
 def gen_pd_holding():
@@ -72,40 +88,86 @@ def gen_pd_holding():
     holdings['R1_p'] = (holdings['R1']/holdings['qty'])+holdings['buy']    
     holdings['R2_p'] = (holdings['R2']/holdings['qty'])+holdings['buy']    
     holdings['R3_p'] = (holdings['R3']/holdings['qty'])+holdings['buy']
-    dict = find_ltp(holdings)
-    holdings['ltp'] = [dict[x] for x in holdings['tikr']]
-    holdings['pl'] = (holdings['ltp']-holdings['buy'])*holdings['qty']
+    holdings['entry'] = [x.strftime("%d-%b-%Y (%H:%M)") for x in holdings['entrydate']]
+    holdings['button_id'] = ["btn_" + str(x) for x in holdings['id']]
     return holdings
 
 def charts(request):
     return HttpResponse("Live charts using Bokeh/Plotly")
-
+from datetime import datetime
 def get_live_time():
     from datetime import datetime
     now = datetime.now() # current date and time
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
     return date_time
 
+def get_or_create_ltp():
+    from sector_analysis.models import Holdings,ltp_tikrs
+    holdings = pd.DataFrame(Holdings.objects.values()).reset_index()
+    tikr_list =list(holdings.tikr.value_counts().index)
+    dict = {}
+    for t in tikr_list:
+        try:
+            obj = ltp_tikrs.objects.get(tikr=t)
+            print(t, "is available")
+            dict[t] = obj.ltp
+            pass
+        except:
+            print(t," not available")
+            ltp_tikrs.objects.create(tikr=t)
+            dict[t] = 0.0
+    return dict
+
 def portfolio(request):
     if request.method=="POST":
-        holdings = gen_pd_holding()
-        # ltp_find = find_ltp(tikr_list)
-        datetime=get_live_time()
-        json_records = holdings.to_json(orient ='records')
-        data = []
-        data = json.loads(json_records)
-        context = {'d': data}
-        return render(request,"sector_analysis/portfolio.html",
-        context)
-    
+        if 'update_ltp' in request.POST:
+            holdings = gen_pd_holding()
+            dict = find_ltp(holdings)
+            holdings['ltp'] = [dict[x] for x in holdings['tikr']]   
+            holdings['pl'] = (holdings['ltp']-holdings['buy'])*holdings['qty']
+            time_happen="Last updated on " + get_live_time()
+            json_records = holdings.to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            context = {'d': data,'time_happen':time_happen}
+
+            return render(request,"sector_analysis/portfolio.html",
+            context)
+        else:
+            time_happen= (list(request.POST.keys()))
+            time_happen = [x for x in time_happen if 'btn' in x ][0]
+            id_ = int(time_happen.replace('btn_',''))
+            # t = "entry " + str(id_) + " deleted with tikr " + Holdings.objects.get(pk=id_).tikr
+            Holdings.objects.get(pk=id_).delete()
+
+            holdings = gen_pd_holding()
+            dict = get_or_create_ltp()
+            holdings['ltp'] = [dict[x] for x in holdings['tikr']]   
+            holdings['pl'] = (holdings['ltp']-holdings['buy'])*holdings['qty']
+           
+
+            # time_happen = time_happen['btn' in time_happen]
+            json_records = holdings.to_json(orient ='records')
+            data = []
+            data = json.loads(json_records)
+            context = {'d': data,'time_happen':time_happen}
+
+            return render(request,"sector_analysis/portfolio.html",
+            context)
+
+    # get_or_create_ltp()
     holdings = gen_pd_holding()
-    datetime=get_live_time()
+    dict = get_or_create_ltp()
+    holdings['ltp'] = [dict[x] for x in holdings['tikr']]
+    holdings['pl'] = (holdings['ltp']-holdings['buy'])*holdings['qty']
     json_records = holdings.to_json(orient ='records') 
     data = [] 
     data = json.loads(json_records) 
-    context = {'d': data} 
+    time_happen = "Values from database"
+    context = {'d': data,'time_happen':time_happen} 
     return render(request,"sector_analysis/portfolio.html",
     context)
+
 
 
     
@@ -121,16 +183,16 @@ def index(request):
             buy = buy,
             qty = qty,
             sl = sl,
-            t=target
+            t=target,
+            entrydate=datetime.now()    ,
         )
         return HttpResponse(str(tikr)+"|"+str(buy)+"|"+str(qty)+"|"+str(sl)+"|"+str(target))
 
     return render(request,"sector_analysis/index.html",
-    {"sectors": sectors,
+    {
+    "sectors": sectors,
     "dict":json.dumps(dict) ,
     "img_path":sectors,
     }
     )
-
-
 
